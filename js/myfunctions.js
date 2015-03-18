f3bb46bba3be6305415975a8773a9ab48b02887a
TimohStudios <timogos.k@live.nl>
2015-03-18 22:16:16 +0100

    BAM! JS!
    That's the JS done!

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,25 +1,33 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  var modal = document.getElementById('js-modal'),
+     cookie = 'seen',
+ exipreDays = 120,
+ expireTime = new Date(),
+  openClass = 'is-open',
+       body = document.body;
 
-	if (! $.cookie('seen') && $('body').hasClass('home')){
-	  $.cookie('seen', 'step1', { expires: 120, path: '/' });
-	  console.log('home origin');
-	}
+  expireTime.setTime(+expireTime + exipreDays * 864e+5);
 
-	if ($.cookie('seen')==="step1" && $('body').hasClass('inner-page')){
-	  $.cookie('seen', 'step2', { expires: 120, path: '/' });
-	  console.log('home > inner ');
-	}
-
-	if ($.cookie('seen')==="step2" && $('body').hasClass('home')){
-	  $('.signup-modal').addClass('is-open');
-	  console.log('home > single > home');
+  function readCookie(name) {
+    return (name = new RegExp('(?:^|;\\s*)' + ('' + name).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&') + '=([^;]*)').exec(document.cookie)) && name[1];
   }
 
-  $('.signup-modal .close').click(function() {
-	  $('.signup-modal').removeClass('is-open');
-	  $.cookie('seen', 'complete', { expires: 120, path: '/' });
-	});
+  
+  if (!readCookie(cookie) && body.classList.contains('home')) {
+    document.cookie = cookie + '=step1; expires=' + expireTime;
+  }
 
-  console.log($.cookie('seen'));
+  if (readCookie(cookie) === "step1" && body.classList.contains('inner-page')) {
+    document.cookie = cookie + '=step2; expires=' + expireTime;
+  }
+
+  if (readCookie(cookie) === "step2" && body.classList.contains('home')) {
+    modal.classList.add(openClass);
+  }
+
+  document.getElementById('js-close').addEventListener('click', function() {
+    modal.classList.remove(openClass);
+    document.cookie = cookie + '=complete; expires=Fri, 31 Dec 9999 23:59:59 GMT';
+  });
 
 });
